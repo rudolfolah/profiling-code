d1de0392c1025cb238fa06d4e02d51be8a278266
Rudolf Olah <rudolf.olah.to@gmail.com>
2024-02-26 00:20:24 -0500

    refactor

--- python/program.py
+++ python/program.py
@@ -4,18 +4,23 @@ import requests
 from collections import Counter
 import tempfile
 
-r = requests.get("https://en.wikipedia.org/wiki/Intuitive_Machines_Nova-C")
-with tempfile.NamedTemporaryFile(delete=False) as temp:
-    temp.write(r.content)
-    temp.flush()
-    temp_filename = temp.name
+def main():
+    r = requests.get("https://en.wikipedia.org/wiki/Intuitive_Machines_Nova-C")
+    with tempfile.NamedTemporaryFile(delete=False) as temp:
+        temp.write(r.content)
+        temp.flush()
+        temp_filename = temp.name
 
-with open(temp_filename, "r") as file:
-    soup = BeautifulSoup(file, "html.parser")
-    words = Counter()
-    for paragraph in soup.find_all("p"):
-        for word in paragraph.text.split():
-            if len(word) > 3:
-                words[word] += 1
+    with open(temp_filename, "r") as file:
+        soup = BeautifulSoup(file, "html.parser")
+        words = Counter()
+        for paragraph in soup.find_all("p"):
+            for word in paragraph.text.split():
+                if len(word) > 3:
+                    words[word] += 1
 
-print(words.most_common(10))
+    print(words.most_common(10))
+
+
+if __name__ == "__main__":
+    main()
