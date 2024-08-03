0ca3a71384bb715f4f36ffaf068d4061f8d2d6bb
Rudolf Olah <rudolf.olah.to@gmail.com>
2024-08-02 22:09:05 -0400

    DTrace for Python

---
+++ python/call_stack.d
@@ -0,0 +1,41 @@
+self int indent;
+
+python$target:::function-entry
+/copyinstr(arg0) == "program.py"/
+{
+        self->trace = 1;
+}
+
+python$target:::function-entry
+/self->trace/
+{
+        printf("%d\t%*s:", timestamp, 15, probename);
+        printf("%*s", self->indent, "");
+        printf("%s:%s:%d\n", basename(copyinstr(arg0)), copyinstr(arg1), arg2);
+        self->indent++;
+}
+
+python$target:::function-return
+/self->trace/
+{
+        self->indent--;
+        printf("%d\t%*s:", timestamp, 15, probename);
+        printf("%*s", self->indent, "");
+        printf("%s:%s:%d\n", basename(copyinstr(arg0)), copyinstr(arg1), arg2);
+}
+
+python$target:::function-return
+/copyinstr(arg0) == "program.py"/
+{
+        self->trace = 0;
+}
+
+python$target:::line
+/*
+/copyinstr(arg0) == "program.py"/
+*/
+{
+        printf("%d\t%*s:", timestamp, 15, probename);
+        printf("%*s", self->indent, "");
+        printf("%s:%s:%d\n", basename(copyinstr(arg0)), copyinstr(arg1), arg2);
+}
